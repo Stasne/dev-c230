42ab1ccbdae9dae43b119567df834f06cd7ad189
Stas Ne <sstusne@yandex.ru>
2022-06-27 04:09:53 +0300

    WIP: task2

--- lab3/src/task2/task2.cpp
+++ lab3/src/task2/task2.cpp
@@ -1,12 +1,35 @@
+#include "task2.h"
+
 #include <chrono>
+#include <future>
 #include <iostream>
 #include <semaphore>
 #include <string_view>
 #include <thread>
 #include <vector>
 
-#include "task2.h"
-
 void Task2::operator()()
 {
+    // WIP
+
+    std::future<int> res;
+    while (true)
+    {
+        auto fut = std::async(std::launch::async, []() {
+            std::string str;
+            std::getline(std::cin, str);
+            std::cout << "got: " << str << std::endl;
+            return std::stoi(str);
+        });
+        try
+        {
+            auto async_res = fut.get();
+            std::cout << "async res: " << async_res << std::endl;
+        }
+        catch (const std::exception& e)
+        {
+            std::cerr << "exception: " << e.what() << '\n';
+            break;
+        }
+    }
 }
